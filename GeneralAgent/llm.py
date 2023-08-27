54dc1f82206500dbf87f55d0563574155ffcd25d
CosmosShadow <lichenarthurml@gmail.com>
2023-08-27 15:28:35 +0800

    prompt翻译成功了

--- GeneralAgent/llm.py
+++ GeneralAgent/llm.py
@@ -3,6 +3,8 @@ import openai
 import os
 import json
 from jinja2 import Template
+from tinydb import TinyDB, Query
+import re
 
 def llm_inference_messages(messages):
     model = os.environ.get('OPENAI_MODEL', 'gpt-3.5-turbo-16k')
@@ -12,6 +14,13 @@ def llm_inference_messages(messages):
     openai.api_base = os.environ.get('OPENAI_API_BASE', 'https://api.openai.com/v1')
     response = openai.ChatCompletion.create(model=model, messages=messages)
     result = response['choices'][0]['message']['content'].strip()
+    
+    if os.environ['REVERSE'] == '1':
+        print('-' * 50 + 'llm_inference_messages' + '-' * 50)
+        for message in messages:
+            print(f'[{message["role"]}] {message["content"]}')
+        print(f'[response] {result}')
+    
     return result
 
 def llm_inference(prompt):
@@ -30,27 +39,30 @@ def embedding_fun(texts):
     return result
 
 def translate_eng(text):
-    system_prompt = [{"role": "system", "content": f"You are a translator, translate the following text to english."}]
+    system_prompt = [{"role": "system", "content": f"You are a translator, translate the following text to english. Do not translate the text in the curly braces."}]
     messages = system_prompt + [{"role": "user", "content": text}]
     return llm_inference_messages(messages)
 
-cached_dict = {}
+
+prompt_en_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'prompt_en.json')
+prompt_db = TinyDB(prompt_en_path)
+
 def cache_translate_eng(text):
-    if text in cached_dict.keys():
-        return cached_dict[text]
+    # 缓存翻译英文
+    result = prompt_db.get(Query().text == text)
+    if result is not None:
+        return result['en']
     else:
-        # TODO: check {{}} 是匹配的
-        result = translate_eng(text)
-        cached_dict[text] = result
-        return result
+        en = translate_eng(text)
+        retry_count = 0
+        while retry_count < 3 and re.findall(r'{{.*?}}', text) != re.findall(r'{{.*?}}', en):
+            en = translate_eng(text)
+            retry_count += 1
+        if re.findall(r'{{.*?}}', text) != re.findall(r'{{.*?}}', en):
+            raise Exception(f"translate_eng failed: {text} -> {en}")
+        prompt_db.insert({'text': text, 'en': en})
+        return en
 
-# def wrape_prompt_to_function(prompt, variables, return_chema):
-
-#     def wrapped_function(text):
-#         system_prompt = [{"role": "system", "content": prompt}]
-#         messages = system_prompt + [{"role": "user", "content": text}]
-#         return function(messages)
-#     return wrapped_function
 
 def prompt_call(prompt, variables, json_schema=None):
     prompt_en = cache_translate_eng("你是一个翻译官，将下面的文本翻译成为{{target}}: {{text}}")
--- test/test_llm.py
+++ test/test_llm.py
@@ -1,2 +1,13 @@
 # 测试llm
 from base_setting import *
+from GeneralAgent.llm import cache_translate_eng
+
+def test_cache_translate_eng():
+    text = "你是一个翻译官，将下面的文本翻译成为{{target}}: {{text}}"
+    en = cache_translate_eng(text)
+    print(en)
+    assert "{{target}}" in en
+    assert "{{text}}" in en
+
+if __name__ == '__main__':
+    test_cache_translate_eng()
