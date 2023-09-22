987dcb6e5939e3baad709ee4fee37933a7b3a158
CosmosShadow <lichenarthurml@gmail.com>
2023-09-22 16:58:32 +0800

    调整agent

--- GeneralAgent/agent.py
+++ GeneralAgent/agent.py
@@ -2,14 +2,15 @@
 
 import logging
 logging.basicConfig(level=logging.INFO)
-
 import os, re
 from jinja2 import Template
+from collections import OrderedDict
 from GeneralAgent.prompts import general_agent_prompt
 from GeneralAgent.llm import llm_inference_messages
 from GeneralAgent.memory import Memory, MemoryNode
 from GeneralAgent.interpreter import CodeInterpreter
 from GeneralAgent.tools import Tools, google_search, wikipedia_search, scrape_web, llm
+
 
 class Agent:
     def __init__(self, workspace, tools=None):
@@ -17,7 +18,7 @@ class Agent:
         if not os.path.exists(workspace):
             os.makedirs(workspace)
         self.memory = Memory(f'{workspace}/memory.json')
-        self.code_workspace = CodeInterpreter(f'{workspace}/code.bin')
+        self.interpreter = CodeInterpreter(f'{workspace}/code.bin')
         self.tools = tools or Tools([google_search, wikipedia_search, scrape_web, llm])
 
     def run(self, content, for_node_id=None, send_message_recall=None):
@@ -74,20 +75,16 @@ class Agent:
         return node
     
     def execute_node(self, node):
-        # is_stop: 有疑问，需要停止
-        # is_stop: 无疑问&无计划，同级别最后一个节点完成时，停止，等待用户最新输入。当没有最新输入时，计算下一个节点
         is_stop = False
-        # 执行node，包括 input、task
-        logging.info('execute_node for <%s>' % node.action)
-        # 构建system prompt
+        
         python_libs = ', '.join([line.strip() for line in open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'r').readlines()])
         python_funcs = self.tools.get_funs_description()
-        # 送入llm的变量
         system_variables = {
             'python_libs': python_libs,
             'python_funcs': python_funcs
         }
         system_prompt = Template(general_agent_prompt).render(**system_variables)
+
         # 获取messages
         # messages = [{'role': 'system', 'content': system_prompt}] + self.memory.get_messages_for_node(node)
         messages = [{'role': 'system', 'content': system_prompt}] + self.memory.get_related_messages_for_node(node)
@@ -100,72 +97,46 @@ class Agent:
         answer_node = MemoryNode(role='system', action='answer', content=llm_response)
         self.memory.add_node_after(node, answer_node)
         
-        # step 01: 运行代码
+        # process: code -> variable -> plan -> ask
         result = self.run_code_in_text(llm_response)
-        # step 02: 替换变量
         result = self.replace_variable_in_text(result)
-        # step 03: 提取计划，且计划放到回复节点中
         has_plan, result = self.extract_plan_in_text(answer_node, result)
-        # step 04: 是否有疑问
-        has_ask, result = self.check_has_ask(result)
-        # 将result中的\n\n替换成为\n，去掉前后空白
+        has_ask, result = check_has_ask(result)
         result = result.replace('\n\n', '\n').strip()
 
-        # 有疑问，就停止等待结果
-        if has_ask: is_stop = True
-
-        # 无计划，完成回复节点
+        if has_ask:
+            is_stop = True
         if not has_plan:
             self.memory.success_node(answer_node)
-
-        # 返回结果 & 是否停止
         return result, answer_node, is_stop
         
     def run_code_in_text(self, string):
-        # 定义正则表达式匹配代码块
         pattern = re.compile(r'```python\n(.*?)\n```', re.DOTALL)
-        # 匹配所有代码块
         matches = pattern.findall(string)
-        # 逐个执行代码块并添加上结果
         for match in matches:
-            # 执行代码块
-            success, sys_out =  self.code_workspace.run_code('replace_python_code_blocks', match)
-            # TODO: 当代码执行失败时，需要看怎么纠正
-            # 干掉代码块 + 在代码后面加上答案
+            success, sys_out = self.interpreter.run_code('replace_python_code_blocks', match)
+            # TODO: when success is False, do something
             string = string.replace('```python\n{}\n```'.format(match), sys_out)
-            # string = string.replace('```python\n{}\n```'.format(match), '```python\n{}\n```\n'.format(match) + '```\n{}```\n'.format(sys_out))
-        # 返回替换后的字符串
         return string
     
     def replace_variable_in_text(self, string):
-        # 定义正则表达式匹配代码块
         pattern = re.compile(r'#\$(.*?)\$#', re.DOTALL)
-        # 匹配所有代码块
         matches = pattern.findall(string)
-        # 逐个执行代码块并替换原字符串
         for match in matches:
-            # 执行代码块
-            # exec(match)
             logging.info('replace_variable_in_text: ' + match)
-            value = self.code_workspace.get_variable(match)
-            # 替换原字符串中的代码块
+            value = self.interpreter.get_variable(match)
             if value is not None:
                 string = string.replace('#${}$#'.format(match), str(value))
-        # 返回替换后的字符串
         return string
 
     def extract_plan_in_text(self, current_node, string):
         has_plan = False
-        # ```plan ``` 块中，每行一个计划
         pattern = re.compile(r'```plan(.*?)```', re.DOTALL)
-        # 匹配所有代码块
         matches = pattern.findall(string)
         for match in matches:
             has_plan = True
-            plan_dict = structure_plan_in_level(match.strip())
-            # plan_dict = structure_plan_in_plain(match.strip())
+            plan_dict = structure_plan(match.strip())
             self.add_plans_for_node(current_node, plan_dict)
-            # 将 ```plan ``` 块 的```plan和```换成空格
             string = string.replace('```plan{}```'.format(match), match)
         return has_plan, string
     
@@ -178,19 +149,16 @@ class Agent:
             self.memory.add_node_in(node, new_node)
             if len(v) > 0:
                 self.add_plans_for_node(new_node, v)
-    
-    def check_has_ask(self, string):
-        # 是否有疑问
-        pattern = re.compile(r'###ask', re.DOTALL)
-        matches = pattern.findall(string)
-        has_ask = len(matches) > 0
-        # 将 ###ask 换成空格
-        string = string.replace('###ask', '')
-        return has_ask, string
-    
-from collections import OrderedDict
 
-def structure_plan_in_level(data):
+
+def check_has_ask(string):
+    pattern = re.compile(r'###ask', re.DOTALL)
+    has_ask = pattern.search(string) is not None
+    string = pattern.sub('', string)
+    return has_ask, string
+
+
+def structure_plan(data):
     # 将plan的字符串结构化，有层级关系
     structured_data = OrderedDict()
     current_section = [structured_data]
@@ -203,14 +171,4 @@ def structure_plan_in_level(data):
             current_section.pop()
         current_section[-1][section] = OrderedDict()
         current_section.append(current_section[-1][section])
-    return structured_data
-
-def structure_plan_in_plain(data):
-    # 将plan的字符串结构化，没有层级关系
-    structured_data = OrderedDict()
-    for line in data.split('\n'):
-        line = line.strip()
-        if not line:
-            continue
-        structured_data[line] = OrderedDict()
     return structured_data
--- test/test_agent.py
+++ test/test_agent.py
@@ -1,32 +1,43 @@
-# 测试Controller
 from base_setting import *
 import os
 import shutil
-from GeneralAgent.controller import Controller
-from GeneralAgent.scratch import SparkNode
+from GeneralAgent.agent import Agent, check_has_ask, structure_plan
+
+
+def test_check_has_ask():
+    has_ask, result = check_has_ask('find the latest 5 news about tesla and save it in variable name_0')
+    assert has_ask is False
+
+    has_ask, result = check_has_ask('###ask where is the moon? ###ask how many times ?')
+    assert has_ask is True
+    assert result == ' where is the moon?  how many times ?'
+
+def test_structure_plan():
+    pass
+
 
 def test_plan_1():
     workspace = './test_workspace'
     if os.path.exists(workspace): shutil.rmtree(workspace)
-    controller = Controller(workspace='./test_workspace')
+    controller = Agent(workspace='./test_workspace')
     controller.run('帮我计算0.99的1000次方', step_count=2)
-    print(controller.scratch)
+    print(controller.memory)
     if os.path.exists(workspace): shutil.rmtree(workspace)
 
 def test_plan_2():
     workspace = './test_workspace'
     if os.path.exists(workspace): shutil.rmtree(workspace)
-    controller = Controller(workspace='./test_workspace')
+    controller = Agent(workspace='./test_workspace')
     controller.run('帮我写一份关于AIGC创业的商业计划', step_count=2)
-    print(controller.scratch)
+    print(controller.memory)
     if os.path.exists(workspace): shutil.rmtree(workspace)
 
 def test_plan_3():
     workspace = './test_workspace'
     if os.path.exists(workspace): shutil.rmtree(workspace)
-    controller = Controller(workspace='./test_workspace')
+    controller = Agent(workspace='./test_workspace')
     controller.run('帮我找一下tesla最新的5条新闻', step_count=2)
-    print(controller.scratch)
+    print(controller.memory)
     if os.path.exists(workspace): shutil.rmtree(workspace)
 
 # test_plan_1、test_plan_2、test_plan_3
@@ -56,10 +67,10 @@ def test_math():
     # 5 return value
     workspace = './test_workspace'
     if os.path.exists(workspace): shutil.rmtree(workspace)
-    controller = Controller(workspace='./test_workspace')
+    controller = Agent(workspace='./test_workspace')
     node, result = controller.run('帮我计算0.99的1000次方', step_count=5)
-    print(controller.scratch)
-    write_code_node = controller.scratch.get_node(2)
+    print(controller.memory)
+    write_code_node = controller.memory.get_node(2)
     code = controller.code_workspace.get_variable(write_code_node.output_name)
     # print(code)
     assert '0.99' in code
@@ -72,9 +83,9 @@ def test_scrape_news():
     # 测试抓取新闻
     workspace = './test_workspace'
     if os.path.exists(workspace): shutil.rmtree(workspace)
-    controller = Controller(workspace='./test_workspace')
+    controller = Agent(workspace='./test_workspace')
     node, result = controller.run('帮我找一下tesla最新的5条新闻，中文返回给我', step_count=5)
-    print(controller.scratch)
+    print(controller.memory)
     print(result)
     if os.path.exists(workspace): shutil.rmtree(workspace)
 
