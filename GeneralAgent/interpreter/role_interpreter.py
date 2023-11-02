4d791db3e7f8c8dcb2d1ea74f4ce43b85acfbef6
CosmosShadow <lichenarthurml@gmail.com>
2023-11-03 00:08:35 +0800

    fix bug: 解决问题需要一步一步进行

--- GeneralAgent/interpreter/role_interpreter.py
+++ GeneralAgent/interpreter/role_interpreter.py
@@ -9,8 +9,8 @@ system_role_prompt = \
 Now: {{now}}
 You are GeneralAgent, a agent on the {{os_version}} computer to help the user solve the problem.
 Remember, you can control the computer and access the internet.
-Solve the task step by step if you need to. If a plan is not provided, explain your plan first simply and clearly.
-You can use the following skills to help you solve the problem directly without explain and ask for permission: 
+If a plan is not provided, explain your plan first simply and clearly.
+You can use the following skills to help you solve the problem directly without explain, without ask for permission: 
 """
 
 # system prompt role
