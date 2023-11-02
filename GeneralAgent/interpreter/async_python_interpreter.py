eb40eafc45d1036574b9f9bc34e1e0d302d3f8f6
CosmosShadow <lichenarthurml@gmail.com>
2023-11-02 20:03:21 +0800

    添加可以执行异步python的解释器

--- GeneralAgent/interpreter/async_python_interpreter.py
+++ GeneralAgent/interpreter/async_python_interpreter.py
@@ -1,9 +1,67 @@
+import re, io, os, sys
+import pickle
+import logging
+from jinja2 import Template
+from .interpreter import Interpreter
+from GeneralAgent.utils import confirm_to_run
 from .python_interpreter import PythonInterpreter
+import asyncio
+
+
+def _remove_unpickleable(namespace):
+    import pickle
+    if '__builtins__' in namespace:
+        namespace.__delitem__('__builtins__')
+    keys = list(namespace.keys())
+    for key in keys:
+        try:
+            pickle.dumps(namespace[key])
+        except Exception as e:
+            namespace.__delitem__(key)
+    return namespace
+
+
+def code_wrap(code, namespace):
+    lines = code.split('\n')
+    code = '\n    '.join(lines)
+    variables = '\n    '.join([f'{name} = globals()[\'{name}\']' for name, value in namespace.items()])
+    content = f"""
+import asyncio
+
+def _remove_unpickleable(namespace):
+    import pickle
+    if '__builtins__' in namespace:
+        namespace.__delitem__('__builtins__')
+    keys = list(namespace.keys())
+    for key in keys:
+        try:
+            pickle.dumps(namespace[key])
+        except Exception as e:
+            namespace.__delitem__(key)
+    for name in ['__name', '__value', '__namespace']:
+        if name in namespace:
+            namespace.__delitem__(name)
+    return namespace
+
+__namespace = None
+
+async def __main():
+    {variables}
+    {code}
+    global __namespace
+    __namespace = _remove_unpickleable(locals().copy())
+"""
+    # print('----------<code>--------')
+    # print(content)
+    # print('----------</code>--------')
+    return content
+
 
 class AsyncPythonInterpreter(PythonInterpreter):
 
-    def run_code(self, code):
+    async def run_code(self, code):
         code = self.add_print(code)
+        code = code_wrap(code, self.globals)
         code = self.import_code + '\n' + code
         globals_backup = self.load()
         logging.debug(code)
@@ -12,7 +70,17 @@ class AsyncPythonInterpreter(PythonInterpreter):
         sys.stdout = output
         success = False
         try:
-            exec(code, self.globals)
+            # exec(code, self.globals)
+            
+            # 异步执行代码
+            local_vars = self.globals
+            exec(code, local_vars, local_vars)
+            main_function = local_vars['__main']
+            await asyncio.create_task(main_function())
+            local_vars = _remove_unpickleable(local_vars)
+            local_vars = local_vars['__namespace']
+            self.globals = local_vars
+
             success = True
         except Exception as e:
             import traceback
