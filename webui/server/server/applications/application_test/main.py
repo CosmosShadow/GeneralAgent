e5e332091944585a025cb57c330f83a677b27a13
CosmosShadow <lichenarthurml@gmail.com>
2023-11-21 10:57:10 +0800

    fix: file sandbox fix bug、file

--- webui/server/server/applications/application_test/main.py
+++ webui/server/server/applications/application_test/main.py
@@ -1,5 +1,11 @@
-from GeneralAgent.agent import Agent
 
-async def main(chat_history, input, file_path, output_callback, file_callback, send_ui):
-    message = 'I receive: ' + input
+async def main(chat_history, input:str, file_path:str, output_callback, file_callback, send_ui):
+    """
+    input: for example, {"data":518102.454}
+    """
+    from GeneralAgent import skills
+    file_path = skills.unique_name() + '.json'
+    with open(file_path, 'w') as f:
+        f.write(input)
+    message = f'I receive: {input}, And write to file [{file_path}](sandbox:{file_path})'
     await output_callback(message)
--- webui/web/src/components/markdown_component.tsx
+++ webui/web/src/components/markdown_component.tsx
@@ -12,65 +12,63 @@ type LinkObject = {
     url: string
   };
   
-function splitStringWithLinks(str: string): LinkObject[] {
-const regex = /(\[.*?\]\(sandbox:\/[^\)]+\))|(\!\[.*?\]\(sandbox:\/[^\)]+\))|([^!]\[.*?\]\(sandbox:\/[^\)]+\))/g; // 匹配 [xxx](sandbox:xxx) 和 ![xx](sandbox:xxx) 格式的链接
-const matches = str.match(regex);
-const result: LinkObject[] = [];
+  function splitStringWithLinks(str: string): LinkObject[] {
+    const regex = /(\[.*?\]\(sandbox:\/?[^\)]+\))|(\!\[.*?\]\(sandbox:\/?[^\)]+\))|([^!]\[.*?\]\(sandbox:\/?[^\)]+\))/g;
+    const matches = str.match(regex);
+    const result: LinkObject[] = [];
 
-if (matches) {
-    let lastIndex = 0;
-    matches.forEach((match) => {
-    const index = str.indexOf(match, lastIndex);
-    const textBefore = str.slice(lastIndex, index);
-    if (textBefore) {
-        result.push({
-        type: 'text',
-        title: textBefore,
-        url: ''
-        });
-    }
-    if (match.startsWith('!')) {
-        result.push({
-        type: 'image',
-        title: match.match(/\[(.*?)\]/)![1],
-        url: match.match(/\(sandbox:\/([^\)]+)\)/)![1]
-        });
-    } else {
-        // 文件结尾是图片格式，type切换成为图片
-        const url = match.match(/\(sandbox:\/([^\)]+)\)/)![1]
-        if (url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') || url.endsWith('.gif')) {
-            result.push({
-                type: 'image',
-                title: match.match(/\[(.*?)\]/)![1],
-                url: url
-            });
-        } else {
-            result.push({
-                type: 'file',
-                title: match.match(/\[(.*?)\]/)![1],
-                url: url
+    if (matches) {
+        let lastIndex = 0;
+        matches.forEach((match) => {
+            const index = str.indexOf(match, lastIndex);
+            const textBefore = str.slice(lastIndex, index);
+            if (textBefore) {
+                result.push({
+                    type: 'text',
+                    title: textBefore,
+                    url: ''
                 });
+            }
+            if (match.startsWith('!')) {
+                result.push({
+                    type: 'image',
+                    title: match.match(/\[(.*?)\]/)![1],
+                    url: match.match(/\(sandbox:\/?([^\)]+)\)/)![1]
+                });
+            } else {
+                const url = match.match(/\(sandbox:\/?([^\)]+)\)/)![1]
+                if (url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') || url.endsWith('.gif')) {
+                    result.push({
+                        type: 'image',
+                        title: match.match(/\[(.*?)\]/)![1],
+                        url: url
+                    });
+                } else {
+                    result.push({
+                        type: 'file',
+                        title: match.match(/\[(.*?)\]/)![1],
+                        url: url
+                    });
+                }
+            }
+            lastIndex = index + match.length;
+        });
+        const textAfter = str.slice(lastIndex);
+        if (textAfter) {
+            result.push({
+                type: 'text',
+                title: textAfter,
+                url: ''
+            });
         }
-        
+    } else {
+        result.push({
+            type: 'text',
+            title: str,
+            url: ''
+        });
     }
-    lastIndex = index + match.length; // 更新 lastIndex
-    });
-    const textAfter = str.slice(lastIndex);
-    if (textAfter) {
-    result.push({
-        type: 'text',
-        title: textAfter,
-        url: ''
-    });
-    }
-} else {
-    result.push({
-    type: 'text',
-    title: str,
-    url: ''
-    });
-}
-return result;
+    return result;
 }
   
 // const input = "I have generated an image of a kitten. You can view the image [here](sandbox:/1edbedad2501.jpg) or ![here](sandbox:/another.jpg). Hello world";
@@ -86,16 +84,16 @@ const MarkdownComponent: React.FC<Props> = (props) => {
     const message = props.message
     const links = splitStringWithLinks(props.message.msg?.trim() as string)
     // console.log(props.message.msg)
-    // console.log(links)
+    console.log(links)
     return (<div style={{ whiteSpace: 'pre-wrap' }} >{
         links.map((item, index)=>{
         // console.log(item)
         if (item.type === 'text') {
             return (<span key={index}>{item.title}</span>)
         } else if (item.type === 'image') {
-            return (<span><br/><ImageComponent image_url={get_chat_file_url(message.bot_id as string, message.chat_id as string, item.url)} /></span>)
+            return (<span key={index}><br/><ImageComponent image_url={get_chat_file_url(message.bot_id as string, message.chat_id as string, item.url)} /></span>)
         } else {
-            return (<span>&nbsp; <FileDownloadCompoent file_path={get_chat_file_url(message.bot_id as string, message.chat_id as string, item.url)} title={item.title}/>&nbsp;</span>)
+            return (<span key={index}>&nbsp; <FileDownloadCompoent file_path={get_chat_file_url(message.bot_id as string, message.chat_id as string, item.url)} title={item.title}/>&nbsp;</span>)
         }
     })
     }</div>)
