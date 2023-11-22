5f72850c3c02a6882ee042db32ce9f7b60bca6c9
CosmosShadow <lichenarthurml@gmail.com>
2023-11-22 14:18:51 +0800

    move agent builder to applications

--- GeneralAgent/skills/agent_builder_2.py
+++ GeneralAgent/skills/agent_builder_2.py
@@ -83,3 +83,58 @@ def create_application_ui(task: str, ui_dir: str = './ui', component_name: str =
         return lib_name, os.path.join(target_dir, 'index.js'), code
     else:
         return None
+    
+
+def update_application_meta_2(
+        application_id:str=None,
+        type:str=None,
+        application_name:str=None,
+        description:str=None,
+        js_name:str=None,
+        js_path:str=None,
+        agent_can_upload_file=None,
+        ) -> None:
+    """
+    Update application meta data. When type is application, you should provide js_name and js_path. When type is agent, you should ignore js_name and js_path.
+    @param application_id: application id, You should name it, example: translat_text, ai_draw
+    @param type: application type, one of ['application', 'agent']. application is a normal application with ui, agent is a agent application with chat interface.
+    @param application_name: application name
+    @param description: application description
+    @param js_name: js file name
+    @param js_path: js file path
+    @param agent_can_upload_file: agent can upload file or not, default is False, if True, agent can upload file to the application
+    @return: None
+    """
+    import os, json
+    from GeneralAgent import skills
+    bot_json_path = os.path.join(skills.get_code_dir(), 'bot.json')
+    if os.path.exists(bot_json_path):
+        with open(bot_json_path, 'r') as f:
+            app_json = json.loads(f.read())
+    else:
+        app_json = {}
+    if application_id is not None:
+        from GeneralAgent import skills
+        bots = skills.load_applications()
+        if application_id in [x['id'] for x in bots]:
+            print(f'application_id ({application_id}) exists. ignore If you are just edit the exist application, or you should change the application_id')
+        app_json['id'] = application_id
+    if type is not None:
+        app_json['type'] = type
+    if application_name is not None:
+        app_json['name'] = application_name
+    if description is not None:
+        app_json['description'] = description
+    app_json['upload_file'] = 'yes'
+    if js_name is not None:
+        app_json['js_name'] = js_name
+    if js_path is not None:
+        app_json['js_path'] = js_path
+    if agent_can_upload_file is not None:
+        app_json['upload_file'] = 'yes'
+    if os.path.exists(os.path.join(skills.get_code_dir(), 'icon.jpg')):
+        app_json['icon'] = 'icon.jpg'
+    else:
+        del app_json['icon']
+    with open(bot_json_path, 'w') as f:
+        f.write(json.dumps(app_json, indent=4))
