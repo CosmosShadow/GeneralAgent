c33558b6da5a800a3d385358558bb5e08ac9dc01
CosmosShadow <lichenarthurml@gmail.com>
2023-12-01 11:51:15 +0800

    Update WebSocketProvider.tsx

--- webui/web/src/components/WebSocketProvider.tsx
+++ webui/web/src/components/WebSocketProvider.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState, PropsWithChildren } from 'react';
+import React, { useEffect, useRef, useState, PropsWithChildren } from 'react';
 import { WebSocketContext } from './WebSocketContext';
 import {WS_HOST} from './api'
 
 interface Props {}
 
 export const WebSocketProvider: React.FC<PropsWithChildren<Props>> = ({ children }) => {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const socketRef = useRef<WebSocket | null>(null);
   const [shouldReconnect, setShouldReconnect] = useState(true);
   let reconnectTimerRef: any = null;
 
   const connectWebSocket = () => {
     const socket = new WebSocket(WS_HOST + '/ws/user/');
-    setSocket(socket);
+    console.log('WebSocket连接OK');
+    socketRef.current = socket;
     socket.onclose = () => {
 			console.log('WebSocket连接已断开');
 			if (shouldReconnect) {
@@ -28,7 +29,7 @@ export const WebSocketProvider: React.FC<PropsWithChildren<Props>> = ({ children
     connectWebSocket()
     return () => {
       setShouldReconnect(false); // 取消重连标志
-      socket && socket.close();
+      socketRef.current && socketRef.current.close();
       if (reconnectTimerRef) {
 				clearTimeout(reconnectTimerRef); // 取消延迟计时器
 			}
@@ -38,24 +39,27 @@ export const WebSocketProvider: React.FC<PropsWithChildren<Props>> = ({ children
   // websocket 心跳
 	useEffect(() => {
 		const timer = setInterval(() => {
-			if (socket && socket.readyState === WebSocket.OPEN) {
+			if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
         console.log('ping')
-				socket.send('ping');
+				socketRef.current.send('ping');
 			}
 		}, 10000);
 		return () => clearInterval(timer);
 	}, []);
 
   const sendMessage = (message: string) => {
-    if (socket?.readyState === WebSocket.OPEN) {
-      socket.send(message);
+    if (socketRef.current?.readyState === WebSocket.OPEN) {
+      socketRef.current.send(message);
     }
   };
 
   const subscribe = (messageHandler: (message: string) => void) => {
-    if (socket) {
-      socket.onmessage = (event) => {
-        if (event.data != 'pong') {
+    if (socketRef.current) {
+      socketRef.current.onmessage = (event) => {
+        // console.log(event);
+        if (event.data == 'pong') {
+          console.log('pong')
+        } else {
           messageHandler(event.data);
         }
       };
