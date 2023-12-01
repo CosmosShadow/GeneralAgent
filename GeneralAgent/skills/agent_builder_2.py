e3fca43052d1a9adb761d19b3ad55d6005672eaa
CosmosShadow <lichenarthurml@gmail.com>
2023-12-01 12:15:30 +0800

    return markdown format to user.

--- GeneralAgent/skills/agent_builder_2.py
+++ GeneralAgent/skills/agent_builder_2.py
@@ -218,6 +218,7 @@ async def main(messages, input, output_callback):
     role_prompt = \"\"\"
 You are a translation agent.
 You complete user requirements by writing python code to call the predefined functions.
+Reponse message in markdown format to user.
 \"\"\"
     functions = [
         skills.translate_text
