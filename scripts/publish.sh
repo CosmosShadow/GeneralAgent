2c6555e45c467841a0b173267439ad8b3cc93a48
CosmosShadow <lichenarthurml@gmail.com>
2023-12-12 11:41:07 +0800

    version 0.1.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,5 +6,5 @@ poetry publish -u lichen -p $PYPI_PASSWORD
 
 # docker
 cd webui/web && npm run build && cd ../../
-docker buildx build --platform linux/amd64,linux/arm64 -f ./docker/Dockerfile -t cosmosshadow/general-agent:0.0.14 . --push
+docker buildx build --platform linux/amd64,linux/arm64 -f ./docker/Dockerfile -t cosmosshadow/general-agent:0.1.0 . --push
 docker buildx build --platform linux/amd64,linux/arm64 -f ./docker/Dockerfile -t cosmosshadow/general-agent:latest . --push
