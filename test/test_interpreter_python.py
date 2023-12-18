9e941a9484290d5de63b17dd7a3de4e4beae9c2d
CosmosShadow <lichenarthurml@gmail.com>
2023-12-18 15:25:21 +0800

    code stack run ok

--- test/test_interpreter_python.py
+++ test/test_interpreter_python.py
@@ -1,16 +1,13 @@
 import os
-import pytest
-import asyncio
-from GeneralAgent.interpreter import SyncPythonInterpreter
+from GeneralAgent.interpreter import PythonInterpreter
 
 
-@pytest.mark.asyncio
 def test_python_interpreter():
     # test add print
     code = """
 x = a + b
 x"""
-    code = SyncPythonInterpreter.add_print(code)
+    code = PythonInterpreter.add_print(code)
     print(code)
     assert code.strip() == """
 x = a + b
@@ -19,7 +16,7 @@ print(x)""".strip()
     serialize_path = './data/test_interpreter.bin'
     if os.path.exists(serialize_path): os.remove(serialize_path)
 
-    interpreter = SyncPythonInterpreter(serialize_path)
+    interpreter = PythonInterpreter(serialize_path)
     sys_stdout, is_stop = interpreter.output_parse('```python\nprint("hello world")\n```')
     assert 'hello world' in sys_stdout.strip()
     assert is_stop is False
@@ -32,3 +29,29 @@ print(x)""".strip()
     sys_stdout, is_stop = interpreter.output_parse('```python\na += 1\n```')
     a = interpreter.get_variable('a')
     assert a == 12
+
+
+def test_stack_code():
+    serialize_path = './data/test_interpreter.bin'
+    if os.path.exists(serialize_path): os.remove(serialize_path)
+    interpreter = PythonInterpreter(serialize_path)
+    code = """
+```python
+a = 10
+code = "```python\\na += 1\\n```"
+interpreter.output_parse(code)
+print(a)
+```
+"""
+    interpreter.set_variable('interpreter', interpreter)
+    sys_stdout, is_stop = interpreter.output_parse(code)
+    print(sys_stdout)
+
+# output:
+# 11
+# python runs result:
+# run successfully
+
+
+if __name__ == '__main__':
+    test_stack_code()
