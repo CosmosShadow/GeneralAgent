9fe08c91be0d5f3dc95d9603b105ab64ad30bfe9
CosmosShadow <lichenarthurml@gmail.com>
2024-01-04 10:29:56 +0800

    Revert "delete dynamic ui"
    This reverts commit 9d14eeed867a91a60e39ecb0fe548e421690cba9.

---
+++ webui/server/server/ts_builder/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['@babel/env', '@babel/preset-react', '@babel/preset-typescript'],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties"],
+    ["@babel/plugin-transform-typescript"],
+  ],
+};
