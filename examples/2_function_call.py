668d2000533433f7677569c96ed301db432afa68
CosmosShadow <lichenarthurml@gmail.com>
2024-06-06 12:12:36 +0800

    函数调用

---
+++ examples/2_function_call.py
@@ -0,0 +1,25 @@
+# 函数调用
+from GeneralAgent.agent import Agent
+from GeneralAgent import skills
+
+# 函数: 获取天气信息
+def get_weather(city: str) -> str:
+    """
+    get weather information
+    @city: str, city name
+    @return: str, weather information
+    """
+    return f"{city} weather: sunny"
+
+
+agent = Agent('你是一个天气小助手', functions=[get_weather])
+agent.user_input('成都天气怎么样？')
+
+# 输出
+# ```python
+# city = "成都"
+# weather_info = get_weather(city)
+# weather_info
+# ```
+# 成都的天气是晴天。
+# 请问还有什么我可以帮忙的吗？
