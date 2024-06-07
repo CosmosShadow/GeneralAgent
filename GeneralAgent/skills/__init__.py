1a2d96f4ebaa26a5b688e9efcf071999030ebc50
CosmosShadow <lichenarthurml@gmail.com>
2024-06-07 12:25:09 +0800

    加载远程函数控制权交给用户

--- GeneralAgent/skills/__init__.py
+++ GeneralAgent/skills/__init__.py
@@ -54,8 +54,6 @@ class Skills:
         fun = self._remote_funs.get(name, None)
         if fun is not None:
             return fun
-        self._load_remote_funs()
-        fun = self._remote_funs.get(name, None)
         if fun is not None:
             return fun
         print('Function {} not found'.format(name))
@@ -64,36 +62,40 @@ class Skills:
     def __init__(self):
         self._local_funs = {}
         self._remote_funs = {}
-        self._global_cache_dict = {}
         self._load_local_funs()
-        self._load_remote_funs()
         self._local_funs['input'] = input
         self._local_funs['print'] = default_output_callback
         self._local_funs['output'] = default_output_callback
 
     def _load_local_funs(self):
+        """
+        加载本目录的函数
+        """
         from GeneralAgent.skills.python_envs import load_functions_with_directory
         self._local_funs = {}
         funcs = load_functions_with_directory(os.path.dirname(__file__))
         for fun in funcs:
             self._local_funs[fun.__name__] = fun
 
-    def _load_remote_funs(self):
-        from GeneralAgent.utils import get_functions_dir
+    def _load_remote_funs(self, functions_code_dir):
+        """
+        加载远程函数
+        @param functions_code_dir: 函数代码目录
+        """
         from GeneralAgent.skills.python_envs import load_functions_with_directory
         self._remote_funs = {}
-        funcs = load_functions_with_directory(get_functions_dir())
+        funcs = load_functions_with_directory(functions_code_dir)
         for fun in funcs:
             self._remote_funs[fun.__name__] = fun
 
     def _search_functions(self, task_description, return_list=False):
         """
-        Search functions that may help to solve the task.
+        搜索函数
+        @param task_description: 任务描述
+        @param return_list: 是否返回列表，默认False，返回字符串s
         """
-        # print('-'*100)
         from .llm_inference import search_similar_texts
         signatures = self._all_function_signatures()
-        # print(signatures)
         results = search_similar_texts(task_description, signatures, top_k=5)
         if return_list:
             return results
@@ -101,9 +103,13 @@ class Skills:
             return '\n'.join(results)
 
     def _all_function_signatures(self):
+        """
+        获取所有函数的签名: 本地函数和远程函数
+        """
         from .python_envs import get_function_signature
         locals = [get_function_signature(fun, 'skills') for fun in self._local_funs.values() if not fun.__name__.startswith('test_')]
         remotes = [get_function_signature(fun, 'skills') for fun in self._remote_funs.values() if not fun.__name__.startswith('test_')]
         return locals + remotes
+
 
 skills = Skills._instance()
