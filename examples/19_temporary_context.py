ee7e10e0b6fd957a152990c32f5d7708230bf147
CosmosShadow <lichenarthurml@gmail.com>
2024-09-18 18:19:07 +0800

    Update 19_temporary_context.py

--- examples/19_temporary_context.py
+++ examples/19_temporary_context.py
@@ -1,7 +1,10 @@
+# 演示临时上下文的用法
 from GeneralAgent import Agent
 
 
-agent = Agent('You are a helpful assistant.', hide_python_code=True)
+agent = Agent('You are a helpful assistant.')
 with agent.temporary_context():
     agent.user_input('My name is Henry.')
 agent.user_input("What's my name?")
+
+# Expect: I don't know your name. How can I help you today?
